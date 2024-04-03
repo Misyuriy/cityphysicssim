6bc28cb34c931e82562042bba387405bbdea2fa4
Misyuriy <andrey.misyurev@yandex.ru>
2024-04-03 13:29:18 +0300

    Added new panel buildings blueprints

--- maps.py
+++ maps.py
@@ -21,6 +21,18 @@ class Blueprints:
         Sprite(path='assets/images/sprites/buildings/test_building.png'),
         Sprite(path='assets/images/sprites/buildings/test_roof.png'))
 
+    p1_5x2 = BuildingBlueprint(
+        Sprite(path='assets/images/sprites/buildings/panel1_building.png'),
+        Sprite(path='assets/images/sprites/buildings/panel1_roof.png'))
+
+    p2_5x2 = BuildingBlueprint(
+        Sprite(path='assets/images/sprites/buildings/panel2_building.png'),
+        Sprite(path='assets/images/sprites/buildings/panel2_roof.png'))
+
+    p1_11x2 = BuildingBlueprint(
+        Sprite(path='assets/images/sprites/buildings/panel_long_building.png'),
+        Sprite(path='assets/images/sprites/buildings/panel_long_roof.png'))
+
 
 class Map:
     def __init__(self, buildings: list[city.Building], road_joints: list[Vector2], road_matrix: list[list]):
@@ -38,11 +50,30 @@ class Map:
 
 test_map = Map(
     buildings=[
-        Blueprints.t1_4x1.get_building(position=[300, -180], height=7),
+        Blueprints.p2_5x2.get_building(position=[300, -300], height=7),
+        Blueprints.p2_5x2.get_building(position=[300, 200], height=7),
     ],
     road_joints=[Vector2(0, 0), Vector2(600, 600), Vector2(0, 600), Vector2(600, 0)],
     road_matrix=[[0, 0, 0, 2],
                  [0, 0, 0, 0],
                  [0, 1, 0, 1],
                  [2, 0, 1, 0]]
+)
+
+test_big_map = Map(
+    buildings=[
+        Blueprints.t1_4x1.get_building(position=[0, 0], height=7),
+        Blueprints.t1_4x1.get_building(position=[600, 0], height=5),
+        Blueprints.t1_4x1.get_building(position=[300, 400], height=3),
+        Blueprints.t1_4x1.get_building(position=[900, 700], rotation=-45, height=5),
+    ],
+    road_joints=[Vector2(-400, 200),
+                 Vector2(640, 200),
+                 Vector2(2000, 200),
+                 Vector2(1440, 1000)
+                 ],
+    road_matrix=[[0, 2, 0, 0],
+                 [2, 0, 2, 1],
+                 [0, 2, 0, 0],
+                 [0, 1, 0, 0]]
 )
